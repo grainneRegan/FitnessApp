0e2c7008da3bad6972fbc762a00703bd95b50935
grainneRegan <g.regan6@nuigalway.ie>
2021-03-25 10:52:27 +0000

    First Commit

---
+++ public/js/register.js
@@ -0,0 +1,17 @@
+function register()
+{
+    let email = document.getElementById('exampleInputEmail1').value
+    let password = document.getElementById('exampleInputPassword1').value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            console.log(user);
+            // ...
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage, errorCode);
+        });
+}
